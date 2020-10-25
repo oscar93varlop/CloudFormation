0d540512ad28e6c6ef79fa90bb147be06fda4e31
oscar93_vl <oscar.vargas@clouxter.com>
2020-10-24 19:48:07 -0500

    agregando modulos de TF

---
+++ instance/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+provider "aws" {
+  region="us-east-1"
+}
+
+resource "aws_security_group" "ssh_conection" {
+  name   = var.sg_name
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+        from_port = ingress.value.from_port
+        to_port = ingress.value.to_port
+        protocol = ingress.value.protocol
+        cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+}
+resource "aws_instance" "terraform_test" {
+      #ami = "ami-0e391adda4e7c88b7"
+      ami = var.ami_id
+      instance_type = var.instance_type
+      tags = var.tags
+      security_groups = ["${aws_security_group.ssh_conection.name}"]
+    }
---
+++ instance/variables.tf
@@ -0,0 +1,16 @@
+variable "ami_id"{
+  default=""
+  description = "Ami id"
+}
+variable "instance_type" {
+
+}
+
+variable "tags" {
+}
+variable "sg_name" {
+
+}
+variable "ingress_rules" {
+
+}
